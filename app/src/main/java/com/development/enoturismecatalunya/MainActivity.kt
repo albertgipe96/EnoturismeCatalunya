87d836434e1654312f38077bdf9bc75e6e6070b3
Albert Gil Peiro <67472509+albertgipe96@users.noreply.github.com>
2024-12-17 22:26:57 +0100

    Initial Setup

---
+++ app/src/main/java/com/development/enoturismecatalunya/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.development.enoturismecatalunya
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.development.enoturismecatalunya.ui.theme.EnoturismeCatalunyaTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            EnoturismeCatalunyaTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    EnoturismeCatalunyaTheme {
+        Greeting("Android")
+    }
+}
