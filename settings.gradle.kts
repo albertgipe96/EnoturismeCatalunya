d3c11479ba134a8568af911358650a163185a693
albertgipe96 <albertgipe96@gmail.com>
2024-12-17 23:13:54 +0100

    AndroidApplicationConventionPlugin in build logic module implementation

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,5 @@
 pluginManagement {
+    includeBuild("build-logic")
     repositories {
         google {
             content {
@@ -19,5 +20,6 @@ dependencyResolutionManagement {
     }
 }
 
-rootProject.name = "Enoturisme Catalunya"
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+rootProject.name = "EnoturismeCatalunya"
 include(":app")
